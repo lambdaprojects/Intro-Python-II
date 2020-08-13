a91594716b2e805c03df969d1b0ff0bf8a30aca5
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2020-08-13 16:51:37 -0500

    Created a player object with constructor for name, item and current room

--- src/player.py
+++ src/player.py
@@ -1,2 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+# Player can have a name, items he collects, room he is in
+
+def player(self, name, items):
+    self.name = name
+    self.items = []
+    self.current_room = 'outside'
